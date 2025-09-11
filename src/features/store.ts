b7367f0daef9dcc7c732e7a26182cde3020c1a56
Soufiane Chajjaoui <schajjaoui@gmail.com>
2025-09-11 14:59:00 +0100

    add middleware to prevent actions when user is not authenticated

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./cart/cartSlice";
-
+import authReducer from "../features/auth/authSlice";
+import cartReducer from "../features/cart/cartSlice";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     cart: cartReducer,
-  }
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authMiddleware),
 });
 
-// Types
+// ✅ définir les types APRÈS l'initialisation
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
